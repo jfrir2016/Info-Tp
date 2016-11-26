9d704bf81fcff98da4c0df45d3f2e5c4660d917c
jfrir2016 <juanreiszer@gmail.com>
2016-11-26 10:54:35 -0300

    listas

--- listas_agregarnodocomentario.c
+++ listas_agregarnodocomentario.c
@@ -1,10 +1,16 @@
 #include "TPO.h"
 
-void AgregarNodoComentario(COMMT *comentario, NodeComment *root)
+void AgregarNodoComentario(COMMT *comentario, NodePub *rootp, NodeComment *rootc)
 {
-  NodeComment current=*root;
-  while(current->nxt!=NULL){
-    current=current->nxt;
-  }current.comentario=*comentario;
-  return;
+  NodeComment *current=rootc;
+  NodePub *currentpub=rootp;
+  do{
+      if(currentpub->publicacion.id==comentario->idP){
+	while(current->nxt!=NULL){
+	  current=current->nxt;
+	}current->comentario=*comentario;
+	return;
+      }if(currentpub->nxt!=NULL)
+	currentpub=currentpub->nxt;
+  }while(currentpub->nxt!=NULL);
 }
