22fdce979c086d0caa335ca1cc7338d2b83b2065
alumno laboratorio <alumno@utn>
2016-11-30 09:11:25 -0300

    -> por .

--- Check.c
+++ Check.c
@@ -11,17 +11,17 @@ int Check (USU **cli,NodeUser *primo)
     return -1;
   }
   
-  e=strcmp((*cli)->Usuario,a->user->Usuario);
+  e=strcmp((*cli)->Usuario,a->user.Usuario);
   while(e!=0&&a->nxt!=NULL)
   {
     a=a->nxt;
-    e=strcmp((*cli)->Usuario,a->user->Usuario);
+    e=strcmp((*cli)->Usuario,a->user.Usuario);
   }
   if(e){
     return -1;
   }
-  if(!strcmp((*cli)->Contra,a->user->Contra))
-    return a->user->id;
+  if(!strcmp((*cli)->Contra,a->user.Contra))
+    return a->user.id;
   
   return -1;
 }
